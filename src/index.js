b3eb180cd38eb80b61370f10add59cc0e3c2932f
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-04 07:32:41 -0700

    Install Mobile Nav Functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './Stylesheets/Style.css';
+import showMobileNav from './Modules/mobile_nav.js';
+
+const navigation = document.getElementById('mobile_nav');
+navigation.addEventListener('click', showMobileNav);

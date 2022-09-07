7604acde6d517cb593c6f291e5281c3a3041e6e9
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-07 01:26:09 -0700

    Add More Data

--- src/Modules/searchCountry.js
+++ src/Modules/searchCountry.js
@@ -20,15 +20,51 @@ const countryData = (country) => {
         } else {
           const thisCountry = document.createElement('article');
           const countryConfirmed = data[countryName].All.confirmed;
-          const countryRecovered = data[countryName].All.recovered;
           const countryDeaths = data[countryName].All.deaths;
+          const countrylife = data[countryName].All.life_expectancy;
+          const countryCapital = data[countryName].All.capital_city;
+          const countryPopulation = data[countryName].All.population;
+          const countryArea = data[countryName].All.sq_km_area;
+          const lastUpdated = data[countryName].All.updated;
           thisCountry.classList.add('userCountry');
           thisCountry.innerHTML = `
-          <h5 id = "countryName">${countryName}</h1>
-          <h6 id = "countryConfirmed">Confirmed Cases: ${countryConfirmed}</h6>
-          <h6 id = "countryRecovered">Recovered Cases: ${countryRecovered}</h6>
-          <h6 id = "countryDeaths">Deaths: ${countryDeaths}</h6>
-          `;
+          <div class="Countrycard intro-sec">
+            <h5 id = "countryName">${countryName}</h5>
+            <div class = "line_logo"></div>
+          </div>
+          <table class="table table-striped">
+          <tbody>
+            <tr>
+              <th scope="row">Confirmed Cases</th>
+              <td>${countryConfirmed}</td>
+            </tr>
+            <tr>
+            <th scope="row">Deaths</th>
+              <td>${countryDeaths}</td>
+            </tr>
+            <tr>
+            <th scope="row">Life Expectancy</th>
+              <td>${countrylife}</td>
+            </tr>
+            <tr>
+            <th scope="row">Capital City</th>
+              <td>${countryCapital}</td>
+            </tr>
+            <tr>
+            <th scope="row">Population</th>
+              <td>${countryPopulation}</td>
+            </tr>
+            <tr>
+            <th scope="row">Area</th>
+              <td>${countryArea}</td>
+            </tr>
+            <tr>
+            <th scope="row">Last Updated</th>
+              <td>${lastUpdated}</td>
+            </tr>
+          </tbody>
+        </table>
+            `;
           const parentNode = document.getElementById('Cases');
           parentNode.appendChild(thisCountry);
         }

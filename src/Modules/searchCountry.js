448e46ae674b19250fe2395b5167e7bd5f02d331
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-07 00:48:33 -0700

    Enhance Form Validation + User Accessability

--- src/Modules/searchCountry.js
+++ src/Modules/searchCountry.js
@@ -1,4 +1,27 @@
 const countryData = (country) => {
-  console.log(country);
+  const realCountry = country.charAt(0).toUpperCase() + country.slice(1);
+  caches.open('covid').then((cache) => {
+    cache.match('https://covid-api.mmediagroup.fr/v1/cases/').then((response) => {
+      response.json().then((data) => {
+        const countries = Object.keys(data);
+        const countryName = countries.find((c) => c === realCountry);
+        if (countryName === undefined) {
+          const alert = document.createElement('div');
+          alert.classList.add('alert', 'alert-danger');
+          alert.innerHTML = `
+        Please Enter A Valid Country.
+      `;
+          const parentNode = document.querySelector('.submition');
+          const refrenceNode = document.querySelector('.submit');
+          parentNode.insertBefore(alert, refrenceNode);
+          setTimeout(() => {
+            parentNode.removeChild(alert);
+          }, 2000);
+        } else {
+          console.log(data[countryName].All.confirmed);
+        }
+      });
+    });
+  });
 };
 module.exports = countryData;
